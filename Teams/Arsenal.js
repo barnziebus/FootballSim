bab6e391f0db32b94b846fe526b17114900be9d6
barnziebus <89733636+barnziebus@users.noreply.github.com>
2024-06-08 09:47:29 +0100

    added arsenal

---
+++ Teams/Arsenal.js
@@ -0,0 +1,55 @@
+let ARSENAL = {
+    city: 'London',
+    stadium: {name: 'Emirates Stadium', capacity: 60704},
+    manager: {name: 'Michal Arteta', style: 'Flamboyant', rating: 7},
+    players: [
+        {forename: 'William',   surname: 'Saliba',       nation: 'FRA', age: 22, pos: 'D', mp: 38, starts: 38, min: 3420, gls:  2, ast:  1, crdY:  4, crdR: 0, tklW: 26, int: 29, err: 3},
+        {forename: 'Declan',    surname: 'Rice',         nation: 'ENG', age: 24, pos: 'M', mp: 38, starts: 37, min: 3225, gls:  7, ast:  8, crdY:  5, crdR: 0, tklW: 51, int: 44, err: 0},
+        {forename: 'Martin',    surname: 'Odegaard',     nation: 'NOR', age: 24, pos: 'M', mp: 35, starts: 35, min: 3091, gls:  8, ast: 10, crdY:  2, crdR: 0, tklW: 17, int: 15, err: 0},
+        {forename: 'Ben',       surname: 'White',        nation: 'ENG', age: 25, pos: 'D', mp: 37, starts: 35, min: 2988, gls:  4, ast:  4, crdY:  8, crdR: 0, tklW: 28, int: 32, err: 1},
+        {forename: 'Bukayo',    surname: 'Saka',         nation: 'ENG', age: 21, pos: 'F', mp: 35, starts: 35, min: 2919, gls: 16, ast:  9, crdY:  4, crdR: 0, tklW: 35, int: 15, err: 1},
+        {forename: 'Gabriel',   surname: 'Magalhaes',    nation: 'BRA', age: 25, pos: 'D', mp: 36, starts: 34, min: 3044, gls:  4, ast:  0, crdY:  4, crdR: 0, tklW: 28, int: 31, err: 1},
+        {forename: 'David',     surname: 'Raya',         nation: 'ESP', age: 27, pos: 'G', mp: 32, starts: 32, min: 2880, gls:  0, ast:  0, crdY:  2, crdR: 0, tklW:  1, int:  0, err: 5},
+        {forename: 'Kai',       surname: 'Havertz',      nation: 'GER', age: 24, pos: 'M', mp: 37, starts: 30, min: 2634, gls: 13, ast:  7, crdY: 11, crdR: 0, tklW: 15, int: 20, err: 0},
+        {forename: 'Gabriel',   surname: 'Martinelli',   nation: 'BRA', age: 22, pos: 'F', mp: 35, starts: 24, min: 2019, gls:  6, ast:  4, crdY:  1, crdR: 0, tklW: 24, int:  5, err: 1},
+        {forename: 'Oleksandr', surname: 'Zinchenko',    nation: 'UKR', age: 26, pos: 'D', mp: 27, starts: 20, min: 1722, gls:  1, ast:  2, crdY:  2, crdR: 0, tklW: 30, int: 20, err: 2},
+        {forename: 'Leandro',   surname: 'Trossard',     nation: 'BEL', age: 28, pos: 'F', mp: 34, starts: 18, min: 1649, gls: 12, ast:  1, crdY:  2, crdR: 0, tklW: 16, int:  8, err: 0},
+        {forename: 'Gabriel',   surname: 'Jesus',        nation: 'BRA', age: 26, pos: 'F', mp: 27, starts: 17, min: 1478, gls:  4, ast:  5, crdY:  6, crdR: 0, tklW: 17, int: 13, err: 0},
+        {forename: 'Jakub',     surname: 'Kiwior',       nation: 'POL', age: 23, pos: 'D', mp: 20, starts: 11, min:  946, gls:  1, ast:  3, crdY:  1, crdR: 0, tklW: 12, int:  8, err: 1},
+        {forename: 'Takehiro',  surname: 'Tomiyasu',     nation: 'JPN', age: 24, pos: 'D', mp: 22, starts: 10, min: 1149, gls:  2, ast:  1, crdY:  2, crdR: 1, tklW:  9, int: 18, err: 0},
+        {forename: 'Eddie',     surname: 'Nketiah',      nation: 'ENG', age: 24, pos: 'F', mp: 27, starts: 10, min: 1082, gls:  5, ast:  2, crdY:  3, crdR: 0, tklW:  7, int:  4, err: 0},
+        {forename: null,        surname: 'Jorginho',     nation: 'ITA', age: 31, pos: 'M', mp: 24, starts: 10, min:  921, gls:  0, ast:  2, crdY:  1, crdR: 0, tklW: 10, int: 12, err: 1},
+        {forename: 'Thomas',    surname: 'Partey',       nation: 'GHA', age: 30, pos: 'D', mp: 14, starts:  9, min:  788, gls:  0, ast:  1, crdY:  3, crdR: 0, tklW: 11, int: 11, err: 0},
+        {forename: 'Aaron',     surname: 'Ramsdale',     nation: 'ENG', age: 25, pos: 'G', mp:  6, starts:  6, min:  540, gls:  0, ast:  2, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 2},
+        {forename: 'Emile',     surname: 'Smith-Rowe',   nation: 'ENG', age: 23, pos: 'M', mp: 13, starts:  3, min:  353, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  1, int:  3, err: 0},
+        {forename: 'Fabio',     surname: 'Vieira',       nation: 'POR', age: 23, pos: 'M', mp: 11, starts:  2, min:  297, gls:  1, ast:  0, crdY:  0, crdR: 1, tklW:  2, int:  2, err: 0},
+        {forename: 'Reiss',     surname: 'Nelson',       nation: 'ENG', age: 23, pos: 'F', mp: 15, starts:  1, min:  268, gls:  0, ast:  0, crdY:  1, crdR: 0, tklW:  6, int:  1, err: 0},
+        {forename: 'Jurrien',   surname: 'Timber',       nation: 'NED', age: 22, pos: 'D', mp:  2, starts:  1, min:   71, gls:  0, ast:  0, crdY:  2, crdR: 0, tklW:  2, int:  0, err: 0},
+        {forename: 'Cedric',    surname: 'Soares',       nation: 'POR', age: 31, pos: 'D', mp:  3, starts:  0, min:   62, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Mohamed',   surname: 'Elneny',       nation: 'EGY', age: 31, pos: 'M', mp:  3, starts:  0, min:   28, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  1, err: 0},
+        {forename: 'Ethan',     surname: 'Nwaneri',      nation: 'ENG', age: 16, pos: 'F', mp:  1, starts:  0, min:   14, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Mauro',     surname: 'Bandeira',     nation: 'POR', age: 19, pos: 'M', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Bradley',   surname: 'Ibrahim',      nation: 'ENG', age: 18, pos: 'M', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Karl',      surname: 'Jakon-Hein',   nation: 'EST', age: 21, pos: 'G', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Myles',     surname: 'Lewis-Skelly', nation: 'ENG', age: 16, pos: 'M', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Charles',   surname: 'Sagoe Jr',     nation: 'ENG', age: 19, pos: 'F', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'James',     surname: 'Sweet',        nation: 'WAL', age: 19, pos: 'D', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Reuell',    surname: 'Walters',      nation: 'ENG', age: 18, pos: 'D', mp:  0, starts:  0, min:    0, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+    ]
+}
+
+// {forename: '', surname: '', nation: '', age: , pos: '', mp: , starts: , min: , gls: , ast: , crdY: , crdR: , tklW: , int: , err: },
+
+/*
+STATS
+  pos = Position
+   mp = Matches Played
+  min = Total Mins Played
+  gls = Goals
+  ast = Assists
+cardY = Yellow Cards
+cardR = Red Cards
+ tlkW = Tackles Won
+  int = Interceptions
+  err = Errors leading to opponent shots
+*/
