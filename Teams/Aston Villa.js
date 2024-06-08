8c1205a280fdee672caf18032738dbca1fe6c7b8
barnziebus <89733636+barnziebus@users.noreply.github.com>
2024-06-08 14:46:40 +0100

    added aston villa stats

---
+++ Teams/Aston Villa.js
@@ -0,0 +1,54 @@
+export let ASTON_VILLA = {
+    city: 'Birmingham',
+    stadium: {name: 'Villa Park', capacity: 42657},
+    manager: {name: 'Unai Emery', style: 'Balanced', rating: 6},
+    players: [
+        {forename: 'Ollie',    surname: 'Watkins',          nation: 'ENG', age: 27, pos: 'F', mp: 37, starts: 37, min: 3217, gls: 19, ast: 13, crdY:  4, crdR: 0, tklW:  7, int:  4, err: 0},
+        {forename: 'Ezri',     surname: 'Konsa',            nation: 'ENG', age: 25, pos: 'D', mp: 35, starts: 35, min: 3069, gls:  1, ast:  0, crdY:  5, crdR: 0, tklW: 32, int: 28, err: 3},
+        {forename: 'John',     surname: 'McGinn',           nation: 'SCO', age: 28, pos: 'M', mp: 35, starts: 35, min: 2999, gls:  6, ast:  4, crdY:  9, crdR: 1, tklW: 35, int: 21, err: 0},
+        {forename: 'Douglas',  surname: 'Luiz',             nation: 'BRA', age: 25, pos: 'M', mp: 35, starts: 35, min: 2993, gls:  9, ast:  5, crdY: 12, crdR: 0, tklW: 38, int: 23, err: 2},
+        {forename: 'Emiliano', surname: 'Martinez',         nation: 'ARG', age: 30, pos: 'G', mp: 34, starts: 34, min: 3015, gls:  0, ast:  0, crdY:  4, crdR: 0, tklW:  0, int:  0, err: 3},
+        {forename: 'Pau',      surname: 'Torres',           nation: 'ESP', age: 26, pos: 'D', mp: 29, starts: 27, min: 2464, gls:  2, ast:  0, crdY:  1, crdR: 0, tklW: 17, int: 14, err: 0},
+        {forename: 'Lucas',    surname: 'Digne',            nation: 'FRA', age: 30, pos: 'D', mp: 33, starts: 27, min: 2410, gls:  1, ast:  3, crdY:  7, crdR: 0, tklW: 35, int: 26, err: 0},
+        {forename: 'Moussa',   surname: 'Diaby',            nation: 'FRA', age: 24, pos: 'F', mp: 38, starts: 25, min: 2174, gls:  6, ast:  8, crdY:  1, crdR: 0, tklW:  5, int:  9, err: 0},
+        {forename: 'Matty',    surname: 'Cash',             nation: 'POL', age: 25, pos: 'D', mp: 29, starts: 23, min: 2142, gls:  2, ast:  2, crdY:  8, crdR: 0, tklW: 24, int: 18, err: 0},
+        {forename: 'Leon',     surname: 'Bailey',           nation: 'JAM', age: 25, pos: 'M', mp: 35, starts: 22, min: 2068, gls: 10, ast:  9, crdY:  5, crdR: 0, tklW:  7, int:  9, err: 0},
+        {forename: 'Diego',    surname: 'Carlos',           nation: 'BRA', age: 30, pos: 'D', mp: 27, starts: 20, min: 1816, gls:  0, ast:  0, crdY:  4, crdR: 0, tklW: 22, int: 13, err: 3},
+        {forename: 'Boubacar', surname: 'Kamara',           nation: 'FRA', age: 23, pos: 'M', mp: 20, starts: 20, min: 1652, gls:  0, ast:  1, crdY:  7, crdR: 1, tklW: 16, int: 21, err: 0},
+        {forename: 'Youri',    surname: 'Tielemans',        nation: 'BEL', age: 26, pos: 'M', mp: 32, starts: 17, min: 1622, gls:  2, ast:  6, crdY:  3, crdR: 0, tklW: 17, int: 15, err: 0},
+        {forename: 'Clement',  surname: 'Lenglet',          nation: 'FRA', age: 28, pos: 'D', mp: 14, starts: 14, min: 1153, gls:  0, ast:  1, crdY:  3, crdR: 0, tklW:  7, int:  7, err: 1},
+        {forename: 'Alex',     surname: 'Moreno',           nation: 'ESP', age: 30, pos: 'D', mp: 21, starts: 11, min: 1031, gls:  2, ast:  0, crdY:  2, crdR: 0, tklW: 18, int: 13, err: 0},
+        {forename: 'Nicolo',   surname: 'Zaniolo',          nation: 'ITA', age: 24, pos: 'F', mp: 25, starts:  9, min:  839, gls:  2, ast:  0, crdY:  7, crdR: 0, tklW: 10, int:  1, err: 0},
+        {forename: 'Jacob',    surname: 'Ramsey',           nation: 'ENG', age: 22, pos: 'F', mp: 16, starts:  8, min:  853, gls:  1, ast:  1, crdY:  2, crdR: 0, tklW:  7, int:  3, err: 0},
+        {forename: 'Morgan',   surname: 'Rogers',           nation: 'ENG', age: 21, pos: 'M', mp: 11, starts:  8, min:  638, gls:  3, ast:  1, crdY:  3, crdR: 0, tklW:  3, int:  3, err: 0},
+        {forename: 'Robin',    surname: 'Olsen',            nation: 'SWE', age: 33, pos: 'G', mp:  5, starts:  4, min:  405, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Jhon',     surname: 'Duran',            nation: 'COL', age: 19, pos: 'F', mp: 23, starts:  3, min:  475, gls:  5, ast:  0, crdY:  5, crdR: 0, tklW:  3, int:  1, err: 0},
+        {forename: 'Tim',      surname: 'Iroegbunam',       nation: 'ENG', age: 20, pos: 'M', mp:  9, starts:  1, min:  164, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  4, int:  2, err: 0},
+        {forename: 'Calum',    surname: 'Chambers',         nation: 'ENG', age: 28, pos: 'M', mp:  5, starts:  1, min:  152, gls:  0, ast:  1, crdY:  1, crdR: 0, tklW:  3, int:  2, err: 0},
+        {forename: 'Leander',  surname: 'Dendoncker',       nation: 'BEL', age: 28, pos: 'M', mp:  8, starts:  1, min:  119, gls:  1, ast:  0, crdY:  0, crdR: 0, tklW:  3, int:  2, err: 1},
+        {forename: 'Tyrone',   surname: 'Mings',            nation: 'ENG', age: 30, pos: 'D', mp:  1, starts:  1, min:   30, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  1, err: 0},
+        {forename: 'Omari',    surname: 'Kellyman',         nation: 'ENG', age: 17, pos: 'F', mp:  2, starts:  0, min:   37, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Philippe', surname: 'Continho',         nation: 'BRA', age: 31, pos: 'M', mp:  2, starts:  0, min:   25, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Bertrand', surname: 'Traore',           nation: 'BFA', age: 27, pos: 'F', mp:  2, starts:  0, min:   11, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  1, int:  0, err: 0},
+        {forename: 'Cameron',  surname: 'Archer',           nation: 'ENG', age: 22, pos: 'M', mp:  1, starts:  0, min:    6, gls:  0, ast:  0, crdY:  1, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Kaine',    surname: 'Kesler-Hayden',    nation: 'ENG', age: 20, pos: 'M', mp:  3, starts:  0, min:    6, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Finley',   surname: 'Munroe',           nation: 'ENG', age: 18, pos: 'M', mp:  1, starts:  0, min:    4, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+        {forename: 'Jaden',    surname: 'Philogene-Bidace', nation: 'ENG', age: 21, pos: 'F', mp:  1, starts:  0, min:    4, gls:  0, ast:  0, crdY:  0, crdR: 0, tklW:  0, int:  0, err: 0},
+    ]
+}
+
+// {forename: '', surname: '', nation: '', age: , pos: '', mp: , starts: , min: , gls: , ast: , crdY: , crdR: , tklW: , int: , err: },
+
+/*
+STATS
+  pos = Position
+   mp = Matches Played
+  min = Total Mins Played
+  gls = Goals
+  ast = Assists
+cardY = Yellow Cards
+cardR = Red Cards
+ tlkW = Tackles Won
+  int = Interceptions
+  err = Errors leading to opponent shots
+*/
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 console.log('Football Sim')
 
 import { ARSENAL } from "./Teams/Arsenal.js"
+import { ASTON_VILLA } from "./Teams/Aston Villa.js"
 
-for (let player of ARSENAL.players) {
-    if (player.gls > 10) {
-        console.log(player.forename + ': ' + player.gls)
+let teams = {'Arsenal': ARSENAL, 'Aston Villa': ASTON_VILLA}
+
+for (let team in teams) {
+    console.log(team)
+    for (let player of teams[team]['players']) {
+        if (player.gls > 9) {
+            console.log(player.surname + ': ' + player.gls)
+            }
     }
 }
